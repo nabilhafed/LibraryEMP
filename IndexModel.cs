7787e70aab0b81bb01efcc31dec2d17dea6e8820
Nabil <hafednabil71@gmail.com>
2024-03-27 11:11:24 +0100

    Ajoutez des fichiers projet.

---
+++ IndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class indexModel
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
