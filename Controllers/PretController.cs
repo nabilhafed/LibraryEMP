bbc8dbb5e00a28e61497362d9fb5b28881a0a77c
topplay020 <topplay020@gmail.com>
2024-04-28 10:25:56 +0100

    Pret Page on Progress

--- Controllers/PretController.cs
+++ Controllers/PretController.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryEMP.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace LibraryEMP.Controllers
 {
@@ -16,17 +17,76 @@ namespace LibraryEMP.Controllers
         [HttpGet]
         [Route("getUserByID")]
         public dynamic? getUserByID(string IdAdherent)
-        {
+        {   
             return _db.Adherents
-            .Where(a => a.IdAdherent.ToUpper() == (IdAdherent.ToUpper()))
-            .Select(a => new
-            {
-                nom = a.Nom,
-                prenom = a.Prenom,
-                idCategorie = a.IdCategorie,
-                etatAdherent = a.EtatAdherent
+            .Where(a => a.IdAdherent.ToUpper() == IdAdherent.ToUpper())
+            .Join(
+                _db.Categories,
+                adherent => adherent.IdCategorie,
+                categor => categor.IdCategorie,
+                (adherent, categor) => new
+                {
+                    nom = adherent.Nom,
+                    prenom = adherent.Prenom,
+                    etatAdherent = adherent.EtatAdherent,
+                    readyDate = categor.DureePret,
+                    //get the max borrow number and current borrow book
+                    documentsAuthorized = categor.NombreDocument,
+                    documentsOnborrow = _db.Prets.Count(p => p.IdAdherent.ToUpper() == adherent.IdAdherent.ToUpper())
+                }
+            )
+            .FirstOrDefault();
+        }
 
-            }).FirstOrDefault();
+        [HttpGet]
+        [Route("getHolidays")]
+        public dynamic? getHolidays()
+        {
+            return _db.JoursFeries.Select(j => new
+            {
+                date = j.DateJourFerie
+            }).ToArray();
+        }
+
+        [HttpGet]
+        [Route("getBookByCoteForBorrow")]
+        public dynamic? getBookByCoteForBorrow(string cote , string IdAdherent)
+        {
+            var book =  _db.Notices
+            .Where(n => n.Cote.ToUpper() == cote.ToUpper())
+            .Select(n => new
+            {
+                titrePropre = n.TitrePropre,
+                idNotice = n.IdNotice,
+                //see if the Adherent is currently borrow that book !!
+                canBeBorrow =   _db.Prets
+                                .Join(_db.Exemplaires,
+                                    pret => pret.IdExemplaire,
+                                    exemplaire => exemplaire.IdExemplaire,
+                                    (pret, exemplaire) => new { Pret = pret, Exemplaire = exemplaire })
+                                .Where(join => join.Pret.IdAdherent.ToUpper() == IdAdherent.ToUpper() && join.Exemplaire.Cote.ToUpper() == cote.ToUpper())
+                                .Count() == 0
+                //see if the Adherent has reserve that book before ?
+                //reservedByHim = _db.Reservations.Any(r => r.Cote.ToUpper() == cote.ToUpper() && r.IdAdherent.ToUpper() == IdAdherent.ToUpper())
+
+            })
+            .FirstOrDefault();
+
+            if (book != null)
+            {
+                if(book.canBeBorrow){
+                    var avilables = _db.Exemplaires
+                    .Where(e => e.Cote.ToUpper() == cote.ToUpper())
+                    .Select(e => new
+                    {
+                        idEtat = e.IdEtat,
+                        idExemplaire = e.IdExemplaire
+                }).ToList();
+                    return new { book, avilables };
+                }
+                return new { book };
+            }
+            return null;
         }
 
     }
--- wwwroot/js/Pret.js
+++ wwwroot/js/Pret.js
@@ -1,36 +1,144 @@
 ﻿//test 4/152   and A/067
-$("#getUserIDButton").on('input',function (e) { 
-    e.preventDefault();
+var Holidays;
+async function getHolidays(){
+    if(!Array.isArray(Holidays))
+        Holidays = (await $.get($("#getHolidaysInput").data('request-url'))).map(day => new Date(Date.parse(day)));
+    return Holidays;
+}
+
+function isSameDate(date1, date2) {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }
+
+
+$("#getUserIDButton").on('input',function () { 
     if($(this).val() == '') return;
     $(this).prop("disabled", true);
     let that = this;
     $.get($(this).data('request-url'), {IdAdherent:$(this).val()},
         function (data) {
             $(that).prop("disabled", false);
+            $("#adherentStateAlert").removeClass("alert-dark").removeClass("alert-success").removeClass("alert-warning").removeClass("alert-danger")
             if (typeof data !== 'undefined') {
-                $("#adherentNameInput").val(data["nom"]);
-                $("#adherentPrenameInput").val(data["prenom"]);
-                
-                $("#adherentStateAlert").removeClass("alert-dark").removeClass("alert-success").removeClass("alert-warning").removeClass("alert-danger")
-                switch(data["etatAdherent"]){
+                $("#adherentNameInput").val(data.nom);
+                $("#adherentPrenameInput").val(data.prenom);
+                switch(data.etatAdherent){
                     //Pénalisé
                     case 1 :  
-                    $("#adherentStateAlert").find("p").text("La personne est en état de Pénalisé à la bibliothèque. Emprunt impossible pour le moment."); 
-                    $("#adherentStateAlert").addClass("alert-danger");
+                        $("#adherentStateAlert").find("p").text("La personne est en état de Pénalisé à la bibliothèque. Emprunt impossible pour le moment."); 
+                        $("#adherentStateAlert").addClass("alert-danger");
                     break;
                     //En règle
                     case 2 :  
-                    $("#adherentStateAlert").find("p").text("La personne est en règle à la bibliothèque. Emprunt autorisé."); 
-                    $("#adherentStateAlert").addClass("alert-success");
-                    $("#bookPart").removeClass("disabled");
+                        let documentToborrow = data.documentsAuthorized - data.documentsOnborrow;
+                        // in case cant borrow more books !!!
+                        if ( documentToborrow <= 0 ){
+                            $("#adherentStateAlert").find("p").text("La personne est en règle à la bibliothèque. mais Il ne peut pas emprunter plus des documents!!");
+                            $("#adherentStateAlert").addClass("alert-warning");
+                            return;
+                        }
+                        $("#adherentStateAlert").find("p").text("La personne est en règle à la bibliothèque. Emprunt autorisé de moins que "+documentToborrow+" documents."); 
+                        $("#adherentStateAlert").addClass("alert-success");
+                        $("#bookPart").removeClass("disabled");
+                        $("#datePart").removeClass("disabled");
+                        $("#readyDateInput").val(new Date().toISOString().slice(0,10));
+                        let returnDate = new Date(Date.now() + data.readyDate * 24 * 60 * 60 * 1000);
+                        // search if the return day is holiday day !
+                        getHolidays().then( Holidays =>{
+                            for (const day of Holidays)
+                                if(isSameDate(returnDate, day))
+                                    returnDate.setDate(returnDate.getDate() +1);
+
+                            $("#returnDateInput").val(returnDate.toISOString().slice(0,10));
+                        });
+                        
                     break;
                     //Suspendu
                     case 3 :  
-                    $("#adherentStateAlert").find("p").text("La personne est suspendue à la bibliothèque. Emprunt indisponible."); 
-                    $("#adherentStateAlert").addClass("alert-warning");
+                        $("#adherentStateAlert").find("p").text("La personne est suspendue à la bibliothèque. Emprunt indisponible."); 
+                        $("#adherentStateAlert").addClass("alert-warning");
                     break;
                 }
+            }else{
+                //rest
+                $("#adherentNameInput").val("XXXXX");
+                $("#adherentPrenameInput").val("XXXXX");
+                //$("#returnDateInput").val("dd/mm/yyyy");
+                //$("#readyDateInput").val("dd/mm/yyyy");
+                $("#submitNewBorrow").prop("disabled", true);
+                $("#adherentStateAlert").find("p").text("veuillez sélectionner Adhérent pour obtenir des informations"); 
+                $("#adherentStateAlert").addClass("alert-dark");
+                $("#bookPart").addClass("disabled");
+                $("#datePart").addClass("disabled");
             }
         }
     );
 });
+
+
+$("#getBookCoteButton").on('input', function () {
+    if($(this).val() == '') return;
+    $(this).prop("disabled", true);
+    let that = this;
+    $.get($(this).data('request-url'), {cote:$(this).val() , IdAdherent:$("#getUserIDButton").val()},
+        function (data) {
+            $(that).prop("disabled", false);
+            $("#bookStateAlert").removeClass("alert-dark").removeClass("alert-primary").removeClass("alert-warning");
+            if (typeof data !== 'undefined') {
+                //check if that document is allredy has been borrowd from the same Adhrent !!!
+                if(data.book.canBeBorrow){
+                    $("#bookStateAlert").addClass("alert-primary");
+                    $("#bookStateAlert").find("p").text("il peut emprunter ce livre"); 
+                    $("#bookTitleInput").val(data.book.titrePropre);
+                    $("#booksAvailable").empty();
+                    for (const exemple of data.avilables) {
+                        let option = $('<option>', {
+                                value: exemple.idEtat,
+                                text: exemple.idExemplaire
+                            })
+                        switch (exemple.idEtat) {
+                            //En cours de traitement
+                            case 0:
+                                $(option).addClass("bg-info").prop("disabled", true);
+                            break;
+                            //Disponible
+                            case 1:
+                                if(exemple.reservedByHim)
+                                $(option).addClass("bg-success");
+                                $("#submitNewBorrow").prop("disabled", false);
+                            break;
+                            //Prêté
+                            case 2:
+                                $(option).addClass("bg-warning").prop("disabled", true);
+                            break;
+                            //Perdu
+                            case 3:
+                                $(option).addClass("bg-danger").prop("disabled", true);
+                            break;
+                        }
+                        $("#booksAvailable").append(option);
+                    }
+                    if ($("#submitNewBorrow").prop("disabled"))
+                        $("#submitNewBorrow").text("Réserve");
+                }else{
+                    $("#bookStateAlert").addClass("alert-warning");
+                    $("#bookStateAlert").find("p").text("il ne peut pas emprunter ce livre parce qu'il l'empruntait comme avant"); 
+                }
+                
+            }else{
+                //rest
+                $("#bookStateAlert").addClass("alert-dark");
+                $("#bookStateAlert").find("p").text("veuillez sélectionner le document pour obtenir le statut"); 
+                $("#bookTitleInput").val("XXXXX");
+                $("#booksAvailable").empty();
+                $("#booksAvailable").append('<option>', {text: "Select Document"});
+                $("#submitNewBorrow").prop("disabled", true);
+            };
+        }
+    );
+});
+
