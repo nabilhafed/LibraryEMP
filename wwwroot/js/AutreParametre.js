3f67ee91e74124568789de6a096b5e0cfe47c711
Nabil <hafednabil71@gmail.com>
2024-05-26 00:09:09 +0100

    kkkk

--- wwwroot/js/AutreParametre.js
+++ wwwroot/js/AutreParametre.js
@@ -1,4 +1,33 @@
-﻿
+﻿//For loading icon
+function loadingTemplate() {
+    return '<i class="fa fa-spinner fa-spin fa-fw fa-2x"></i>'
+}
+
+// for edit and delete buttons
+function operateFormatter(value, row, index) {
+    return [
+        '<div class="text-centers w-100 d-flex justify-content-around">',
+        '<a class="edit"><i class="btn fa-solid fa-pen text-primary"></i></a>',
+        '<a class="remove"><i class="btn fa fa-trash text-danger"></i></a>',
+        '</div>'
+    ].join('');
+}
+// for edit and delete buttons
+window.operateEvents = {
+    'click .edit': function (e, value, row, index) {
+        $("#modifiedFouries").modal('show')
+    },
+    'click .remove': function (e, value, row, index) {
+        $("#FeriesTable").bootstrapTable('remove', {
+            field: 'dateJourFeries',
+            values: [row.dateJourFeries]
+        })
+    }
+}
+
+
+
+/*
 $("#HiddenButton").on('click', function () {
     $.get($(this).data('request-url'), {})
         .done(function (data) {
@@ -24,7 +53,7 @@ $("#HiddenButton").on('click', function () {
         });
 });
 $("#HiddenButton").click();
-
+*/
  
 
 
